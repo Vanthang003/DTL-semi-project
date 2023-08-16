2533b1213b053c28775acb4fa97c0a56574d5124
Ngô Văn Thắng <115099950+Vanthang003@users.noreply.github.com>
2023-08-16 16:57:42 +0700

    Update search.blade.php

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -32,7 +32,7 @@
 								<li class="page-item"><a class="page-link" href="#">2</a></li>
 								<li class="page-item"><a class="page-link" href="#">3</a></li>
 								<li class="page-item">
-									<a class="page-link" href="#" aria-label="Next">
+									<a class="page-link" href="#" aria-label="Next" >
 										<span aria-hidden="true">&raquo;</span>
 										<span class="sr-only">Next</span>
 									</a>
