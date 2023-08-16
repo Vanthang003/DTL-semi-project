ace4b1bc15810d885a340964a3823c9e630e8b3c
Ngô Văn Thắng <115099950+Vanthang003@users.noreply.github.com>
2023-08-16 16:57:10 +0700

    Update email.blade.php

--- resources/views/frontend/email.blade.php
+++ resources/views/frontend/email.blade.php
@@ -103,7 +103,7 @@
 								<b>Quý khách đã đặt hàng thành công!</b><br />
 								• Sản phẩm của Quý khách sẽ được chuyển đến Địa chỉ có trong phần Thông tin Khách hàng của chúng Tôi sau thời gian 2 đến 3 ngày, tính từ thời điểm này.<br />
 								• Nhân viên giao hàng sẽ liên hệ với Quý khách qua Số Điện thoại trước khi giao hàng 24 tiếng.<br />
-								<b><br />Cám ơn Quý khách đã sử dụng Sản phẩm của Công ty chúng Tôi!</b>
+								<b><br />Cám ơn Quý khách đã sử dụng Sản phẩm của Công ty chúng Tôi! </b>
 							</p>
 						</div>
 					</div>					
